b5fb97c1056446157851dcc95edb996334655991
huyenanh-nguyen <ng_huyenanh@mailbox.org>
2025-09-21 21:28:11 +0200

    metadata

--- Onesidecoupling/method.py
+++ Onesidecoupling/method.py
@@ -140,6 +140,63 @@ class Fastplots(OnesidedCoupling):
             plt.show()
         
         return None
+    
+
+    def metadata_from_timeseries(self, variable : str, rounding : int):
+        """
+        Printing out the period, frequency, angular frequency and the mean of the last 10 peaks as Amplitude.
+        its empiric measured.
+
+        Args:
+            variable (str): "x", "y", "p" or "q" 
+            rounding (int): digit to round, if rounding is 3, the value will get rounded 3 digit
+
+        Returns:
+            Metadata: print out the Period, frequency, angular frequency and the mean of the last 10 Amplitudes of the timeseries
+        """
+        par = self.par
+        t = self.t
+        k = self.k
+        keep = self.t_keep
+        gamma = self.gamma
+        alpha = self.alpha
+        mu = self.mu
+        beta = self.beta
+
+        if variable == "x":
+            x_period = OnesidedCoupling(par, t, keep, k, mu, gamma, alpha, beta).period(10)[0]
+            x_amp_vdp = np.mean(OnesidedCoupling(par, t, keep, k, mu, gamma, alpha, beta).find_peaks_max()[0][1]['peak_heights'])
+            print("x Period van der Pol: ", round(x_period, rounding))
+            print("x Frequency van der Pol: ", round(1/x_period, rounding))
+            print("x Angular frequency van der Pol: ", round(2 * np.pi * (1/x_period), rounding))
+            print("x Amplitude van der Pol: ", round(x_amp_vdp, rounding))
+
+
+        elif variable == "y":
+            y_period = OnesidedCoupling(par, t, keep, k, mu, gamma, alpha, beta).period(10)[1]
+            y_amp_vdp = np.mean(OnesidedCoupling(par, t, keep, k, mu, gamma, alpha, beta).find_peaks_max()[1][1]['peak_heights'])
+            print("y Period Duffing: ", round(y_period, rounding))
+            print("y Frequency Duffing: ", round(1/y_period, rounding))
+            print("y Angular frequency Duffing: ", round(2 * np.pi * (1/y_period), rounding))
+            print("y Amplitude Duffing: ", round(y_amp_vdp, rounding))
+
+        elif variable == "p":
+            p_period = OnesidedCoupling(par, t, keep, k, mu, gamma, alpha, beta).period(10)[2]
+            p_amp_vdp = np.mean(OnesidedCoupling(par, t, keep, k, mu, gamma, alpha, beta).find_peaks_max()[2][1]['peak_heights'])
+            print("p Period van der Pol: ", round(p_period, rounding))
+            print("p Frequency van der Pol: ", round(1/p_period, rounding))
+            print("p Angular frequency van der Pol: ", round(2 * np.pi * (1/p_period), rounding))
+            print("p Amplitude van der Pol: ", round(p_amp_vdp, rounding))
+ 
+        elif variable == "q":
+            q_period = OnesidedCoupling(par, t, keep, k, mu, gamma, alpha, beta).period(10)[3]
+            q_amp_vdp = np.mean(OnesidedCoupling(par, t, keep, k, mu, gamma, alpha, beta).find_peaks_max()[3][1]['peak_heights'])
+            print("q Period Duffing: ", round(q_period, rounding))
+            print("q Frequency Duffing: ", round(1/q_period, rounding))
+            print("q Angular frequency Duffing: ", round(2 * np.pi * (1/q_period), rounding))
+            print("q Amplitude Duffing: ", round(q_amp_vdp, rounding))
+
+        return None
 
 
 
@@ -164,4 +221,4 @@ alpha = 0.2
 
 hm = Fastplots(par, t, keep, k, mu, gamma, alpha, beta, path)
 
-print(hm.timeseries_singleplot("p", "test", [0, 400], None, False))
+print(hm.metadata_from_timeseries("y", 3))
